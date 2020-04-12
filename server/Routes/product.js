6a248bafbfdd62a3fa52c3ff420137dfd1fafdb0
qianhao-girl <shenmedongdong85@gmail.com>
2020-04-12 22:23:14 +0800

    comment router

--- server/Routes/product.js
+++ server/Routes/product.js
@@ -2,12 +2,18 @@ const router = require('express').Router();
 const Product = require("../database/models/product");
 const { auth } = require("../middleware/auth");
 const productController = require('../controllers/product');
+const VideoControllers = require('../controllers/video');
 
 
 router.post('/uploadImage', auth, productController.postUploadImage);
+router.post('/uploadVideo', auth, VideoControllers.postUploadVideo);
 
 router.post('/getProducts',productController.getProducts);
 
 router.get('/product_by_id', productController.getProductById);
 
+router.post('/saveComment', auth, productController.postComment);
+router.get('/getComments', productController.getComments);
+
 module.exports = router;
+
