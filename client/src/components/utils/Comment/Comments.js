49e05c6663bb7176f505fd563cb52aa3d875bca4
qianhao-girl <shenmedongdong85@gmail.com>
2020-04-12 21:56:52 +0800

    hard move file from video-dev branch to master branch

---
+++ client/src/components/utils/Comment/Comments.js
@@ -0,0 +1,68 @@
+import React,{ useState} from 'react';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
+import SingleComment from './_SingleComment';
+
+
+function Comments(props) {
+    const user = useSelector(state => state.user);
+    const [Content, setContent] = useState("");
+
+    const onInputChangeHandler = (event) => {
+        setContent(event.currentTarget.value);
+    };
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+
+        const variables = {
+            content: Content,
+            writer: user.UserData._id,
+            postId: props.postId,
+        }        
+        axios.post('/api/product/saveComment', variables).then(
+            response => {
+                if(response.data.success){
+                    setContent("");
+                    props.refreshFunction(response.data.result);
+                }else{
+                    console.log(response.data.err);
+                }
+            }
+        )
+    }
+
+    return (
+        <div>
+            <h2>replies</h2>
+            <hr></hr>
+            {props.commentLists && props.commentLists.map((comment,index) =>{
+                if(comment.replyTo){
+                    return null;
+                }else{
+                    return (
+                        <React.Fragment key={"comment"+index}>
+                            <SingleComment 
+                                comment={comment} postId={props.postId}
+                                refreshFunction={props.refreshFunction}
+                            />
+                        </React.Fragment>
+                    )
+                }
+            })}
+            {console.log("comments: ", props.commentLists)}
+            <form onSubmit={onSubmit}>
+                <input type="text" placeholder="write your comment"
+                    value={Content}
+                    onChange={onInputChangeHandler}
+                />
+                <button 
+                    disabled={!user.UserData || !user.UserData._id}
+                    onClick={onSubmit}
+                >submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default Comments
